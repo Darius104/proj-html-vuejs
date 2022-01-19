93f9644aeea805032ddd86882431f6f275baee91
Darius104 <comandarius@gmail.com>
2022-01-19 12:49:23 +0100

    Last push, sezione main quasi finita

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <Header :element="headerArray"/>
-    <Main :element="sottoHeaderArray"/>
+    <Main :element="sottoHeaderArray" :playlist="playlistArray" :subscribe="subscribeArray" :buy="buyArray"/>
   </div>
 </template>
 
@@ -42,19 +42,76 @@ export default {
 
       sottoHeaderArray: [
         {
-          image: './assets/drink.jpg',
+          image: require('./assets/drink.jpg'),
           title: "Crossfit",
           subtitle: "Push your limits"
         },
         {
-          image: './assets/box1-2x.jpg',
+          image: require('./assets/box1-2x.jpg'),
           title: 'New gym apparel',
           subtitle: 'Look good, feel good'
         },
         {
-          image: './assets/box3-2x.jpg',
+          image: require('./assets/box3-2x.jpg'),
           title: 'Team training',
           subtitle: 'Find a partner'
+        }
+      ],
+
+      playlistArray: [
+        {
+          image: require('./assets/video2-2x.jpg'),
+          title: "Thighs & glute workout",
+          subtitle: "Increase your mobility"
+        },
+        {
+          image: require('./assets/video7-2x.jpg'),
+          title: "Lift, firm & perk up",
+          subtitle: "Feel young again"
+        },
+        {
+          image: require('./assets/video9-2x.jpg'),
+          title: "Slim & tirm your waist",
+          subtitle: "Shed those extra pounds"
+        }
+      ],
+
+      subscribeArray: [
+        {
+          title: "Team training",
+          subtitle: "Find a parner",
+          desc: "Vesribulum, curabitur eu sem nibh ultrices sit nulla adipiscing. Nisl sit fames amet senecrus eget sed duis vehicula. Tristique.",
+          span: "Find a partner >"
+        },
+        {
+          title: "Crossfit workout",
+          subtitle: "Push your limits",
+          desc: "Vesribulum, curabitur eu sem nibh ultrices sit nulla adipiscing. Nisl sit fames amet senecrus eget sed duis vehicula. Tristique.",
+          span: "Learn about Crossfit >"
+        },
+        {
+          title: "Weight loss plan",
+          subtitle: "Shed those pounds",
+          desc: "Vesribulum, curabitur eu sem nibh ultrices sit nulla adipiscing. Nisl sit fames amet senecrus eget sed duis vehicula. Tristique.",
+          span: "Schedule a workout >"
+        }
+      ],
+
+      buyArray: [
+        {
+          image: require('./assets/blog4-2x-400x258.jpg'),
+          title: "The best protein shake",
+          subtitle: "By admin | November 26th, 2019 | Gym"
+        },
+        {
+          image: require('./assets/blog1-2x-400x258.jpg'),
+          title: "Ultimate cardio workout",
+          subtitle: "By admin | November 26th, 2019 | Gym"
+        },
+        {
+          image: require('./assets/blog3-2x-400x258.jpg'),
+          title: "New juices available now",
+          subtitle: "By admin | November 26th, 2019 | Gym"
         }
       ]
     }
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -22,9 +22,128 @@
                 </div>
             </div>
         </div>
-
         <div class="challenge-bk">
+            <div class="_container">
+                <div class="container-challenge-card">
+                    <div class="challenge-card" v-for="(el, index) in element" :key="index">
+                        <img class="challenge-img" :src="el.image" alt="">
+                        <div class="challenge-card-text">
+                            <h2>{{el.title}}</h2>
+                            <h3>{{el.subtitle}}</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="main-title-challenge">
+                <h2>
+                    "How you respond to the challenge in the second half <br>
+                    will determine what you become after the game, <br>
+                    whether you are a winner or a loser."
+                </h2>
+                <div class="subheader-profile">
+                    <img class="subheader-profile-picture" src="../assets/avatar-1-2x.jpg" alt="">
+                    <span id="nickname">Brad Hohnson</span>
+                    <span id="town">Las Vegas</span>
+                </div>
+            </div>
+        </div>
+        <div class="section-container">
+            <div class="row">
+                <div class="col-6">
+                    <div class="card-left-training">
+                        <img class="training" src="../assets/service15-2x.jpg" alt="">
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="card-right-training">
+                        <span id="firstSpan">Tune up your workouts</span>
+                        <h2>The best health & fitness advice online</h2>
+                        <div class="youTube-card">
+                            <img class="youtube-logo" src="../assets/unnamed.jpg" alt="">
+                            <div>
+                                <span>ThemeFusion <br> </span>
+                                <div class="youtube-channel"><i class="fab fa-youtube"></i> Youtube</div>
+                                <div class="subscribers">999+</div>
+                            </div>
+                        </div>
+                        <p id="secondP">Arcu eu facilisi ut quisque placerat nunc habitant. Megna semper mauris, venenatis, leo integer sit pellentesque.</p>
+                        <div class="btn youtubeBTN"> <i class="fab fa-youtube"></i> Visit my YouTube channel </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
+        <div class="_container">
+            
+            <div class="row">
+                <div class="col-10">
+                    <h2>Featured playlist</h2>
+                </div>
+                <div class="col-2">
+                    <span>View all videos ></span>
+                </div>
+            </div>
+
+            <div class="featured-playlist">
+                <div class="card-playlist" v-for="(el, index) in playlist" :key="index">
+                    <img class="playlist-image" :src="el.image" alt="">
+                    <div class="playlist-text">
+                        <h4>{{el.title}}</h4>
+                        <h5>{{el.subtitle}}</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="jambo-subscribe">
+            <div class="_container">
+                <div class="text-subscribe">
+                    <span> Join our mailing list today </span>
+                    <h2> Insider offers & flash sales in your <br>
+                        inbox every week
+                    </h2>
+                    <div class="input-subscribe">
+                        <input type="email" placeholder="Insert your email ...*">
+                        <div class="_btn">Subscribe</div>
+                    </div>
+                    <p>
+                        Curabitur non nulla sit amet nisl tempus convallis quis ac lectus dolor sit amet, <br> consectetur
+                        adipiscing elit sed porttitor lectus
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="cards-subscribe-container">
+            <div class="_container">
+                <div class="card-subscribe" v-for="(ele, index) in subscribe" :key="index">
+                    <h3>{{ele.title}}</h3>
+                    <h4>{{ele.subtitle}}</h4>
+                    <p>{{ele.desc}}</p>
+                    <span>{{ele.span}}</span>
+                </div>
+            </div>
+        </div>
+
+        <div class="_container">
+            <div class="title-subscribe">
+                <h4>Latest articles</h4>
+                <h5>Read all aricles ></h5>
+            </div>
+            <div class="container-cards-subscribe">
+                <div class="card-buy" v-for="(e, index) in buy" :key="index">
+                    <img :src="e.image" alt="">
+                    <h2>{{e.title}}</h2>
+                    <span>{{e.subtitle}}</span>
+                </div>
+            </div>
+        </div>
+
+        <div class="last-jambo">
+            <div class="jambo-text">
+                <h2>"I've faild over and over again in my <br> life. And that is why I succeed."</h2>
+                <p>Arcu auctor gravida nisl, congue sit nisi tincidunt eget proin. in lacinia lacus donec sed massa in ipsum eros, tristique. Gravida suspendisse etiam in iaculis</p>
+            </div>
         </div>
     </main>
 </template>
@@ -33,7 +152,10 @@
 export default {
     name: "Main",
     props: {
-        element: Array
+        element: Array,
+        playlist: Array,
+        subscribe: Array,
+        buy: Array
     }
 }
 </script>
@@ -107,6 +229,262 @@ export default {
 }
 
 .challenge-bk{
-    
+    width: 100%;
+    height: 900px;
+    background: linear-gradient(300deg, red, blue);
+    position: relative;
+    .container-challenge-card{
+        display: flex;
+    }
+    .challenge-card{
+        position: relative;
+        top: -60px;
+        width: 30%;
+        margin: auto;
+
+        .challenge-img{
+            width: 100%;
+            border-radius: 5px 5px 40px 5px;
+            overflow: hidden;
+            cursor: pointer;
+        } 
+        .challenge-card-text{
+            position: absolute;
+            left: 20px;
+            bottom: 20px;
+            
+            h2{
+                color: white;
+            }
+            h3{
+                color: $yellow
+            }
+        }
+    }
+
+    .main-title-challenge{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        h2{
+            color: $white;
+            font-size: 50px;
+            margin-bottom: 100px;
+        }
+        .subheader-profile-picture{
+            border-radius: 50%;
+            overflow: hidden;
+            width: 20%;
+        }
+        #nickname{
+            color: $white;
+            font-weight: bold;
+            margin: 0 30px;
+            font-size: 20px;
+        }
+        #town{
+            color: $gray;
+            font-weight: bold;
+            font-size: 20px;
+        }
+    }
+}
+.row{
+    width: 100%;
+}
+.section-container{
+    position: relative;
+    top: -50px;
+    .card-left-training{
+        .training{
+            width: 80%;    
+            border-radius: 5px 5px 40px 5px;
+            overflow: hidden;
+        } 
+    }
+    .card-right-training{
+        margin-top: 150px;
+        #firstSpan{
+            color: $semiBlue;
+            font-size: 30px;
+        }
+        h2{
+            font-size: 60px;
+        }
+        .youtube-logo{
+            display: inline-block;
+            margin-right: 10px;
+        }
+        .youTube-card{
+            display: flex;
+            margin: 30px 0;
+            .youtube-channel{
+                display: inline-block;
+                background-color: red;
+                border: 1px solid gray;
+                border-radius: 2px;
+                padding: 0 5px;
+            }
+            .subscribers{
+                display: inline-block;
+                background-color: white;
+                border-right: 1px solid gray;
+                border-top: 1px solid gray;
+                border-bottom: 1px solid gray;
+                border-radius: 2px;
+                padding: 0 3px;
+                width: 40px;
+            }
+        }
+        #secondP{
+            font-size: 25px;
+            color: $gray;
+            margin-bottom: 80px;
+        }
+        .youtubeBTN{
+            background-color: red;
+            border-radius: 50px;
+            padding: 15px 40px;
+            color: white;
+
+            &:hover{
+                background-color: #ff414b;
+                cursor: pointer;
+            }
+        }
+    } 
+}
+.featured-playlist{
+    margin-top: 50px;
+    display: flex;
+
+    .card-playlist{
+        width: 30%;
+        margin-right: 50px;
+    }
+    .playlist-image{
+        width: 100%;
+        margin-bottom: 20px;
+        border-radius: 5px 5px 40px 5px;
+        overflow: hidden;
+    }
+    .playlist-text{
+        text-align: center;
+
+        h5{
+            color: $semiBlue;
+        }
+    }
+}
+.jambo-subscribe{
+    background-image: url('../assets/banner1-2x.jpg');
+    width: 100%;
+    height: 800px;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position-x: right;
+    margin-top: 100px;
+
+    .text-subscribe{
+        height: 800px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+
+        span{
+            color: #ff414b;
+            font-size: 25px;
+            font-weight: bold;
+        }
+        h2{
+           color: $white; 
+           font-size: 60px;
+           margin-bottom: 40px;
+        }
+        .input-subscribe{
+            margin-bottom: 40px;
+
+            input{
+                padding: 20px;
+                border-radius: 50px;
+                border: none;
+                width: 40%;
+            }
+            ._btn{
+                background-color: $semiBlue;
+                padding: 20px;
+                width: 15%;
+                text-align: center;
+                font-weight: bold;
+
+                &:hover{
+                    cursor: pointer;
+                    background-color: #ff414b;
+                }
+            }
+        }
+        p{
+            font-size: 25px;
+            color: $gray;
+        }
+    }
+}
+.cards-subscribe-container{
+    background-color: #060608;
+    width: 100%;
+    color: white;
+
+    ._container{
+        display: flex;
+        justify-content: space-between;
+
+    }
+    .card-subscribe{
+        width: calc(100% / 3.5);
+        text-align: center;
+        border: 5px solid $semiBlue;
+        border-radius: 10px;
+        padding: 100px 30px;
+        display: inline-block;
+        margin: 100px 0;
+
+        h3{
+            color: $white;
+        }
+        h4{
+            color: $yellow;
+            margin-bottom: 30px;
+        }
+        span{
+            font-weight: bold;
+        }
+    }
+}
+
+._container{
+    margin-bottom: 100px;
+
+    .title-subscribe{
+        display: flex;
+        justify-content: space-between;
+        margin-top: 120px;
+        margin-bottom: 30px;
+    }
+    .container-cards-subscribe{
+        display: flex;
+        justify-content: space-between;
+    }
+    .card-buy{
+        width: calc(100% / 3.5);
+        h2{
+            margin-left: 20px;
+            margin-top: 20px;
+        }
+        span{
+            margin-left: 20px;
+        }
+    }
 }
 </style>
